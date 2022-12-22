37bc3b84da58f450addbce50563b20f830c33c31
satwinder.singh <satwinder.singh@seb.se>
2022-12-22 11:08:31 +0100

    Updated with integration to gcp secret manager

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hello-world-gcp
+
+1.  Create GCP Service account and add role
+Secret Manager Secret Accessor
+2. Create Kubernetes service account
+3. Connect Kubernetes service account to Gcp Service Account using WorkLoadIdentity
+4. Use the KSA in the deployment.yaml file
+
+https://cloud.google.com/kubernetes-engine/docs/tutorials/workload-identity-secrets
+
+https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity#gcloud_4
+
--- src/main/java/com/example/helloworldgcp/rest/HelloWorldController.java
+++ src/main/java/com/example/helloworldgcp/rest/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.helloworldgcp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/v1")
 public class HelloWorldController {
 
+    @Value("${application.custom.message}")
+    private String message;
+
+    @Value("${application.api-key}")
+    private String apiKey;
+
     @GetMapping(value = "helloWorld")
     public ResponseEntity<String> helloWorld(){
-        System.out.println("GET Hello World called !!!");
-        return ResponseEntity.ok("Hello World !");
+        System.out.println("GET Hello World called !!!, Key = " + apiKey);
+        return ResponseEntity.ok(message + " and key = " + apiKey);
     }
 
 }
